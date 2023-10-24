d1b0544f381ce23b088fc9a13a0804d2acadfd0a
Huzaifa-Sultan <siddhuzaifa43@gmail.com>
2023-10-25 01:56:49 +0500

    first commit

---
+++ app.ts
@@ -0,0 +1,57 @@
+import inquirer from "inquirer";
+
+class Student {
+  name: string;
+  constructor(n: string) {
+    this.name = n;
+  }
+}
+
+class Person {
+  stucents: Student[] = [];
+
+  addStudent(obj: Student) {
+    this.stucents.push(obj);
+  }
+}
+
+let persons = new Person();
+
+let programmeStart = async (persons: Person) => {
+  do {
+    console.log("Welcome guest");
+
+    let ans = await inquirer.prompt({
+      type: "list",
+      message: "Who would you like to talk",
+      name: "select",
+      choices: ["By your self", "Student"],
+    });
+    if (ans.select == "By your self") {
+      console.log("Hello i am talking myself");
+    }
+    if (ans.select == "Student") {
+      let ans = await inquirer.prompt({
+        type: "input",
+        message: "Which student you would like to talk",
+        name: "student",
+      });
+
+      let student = persons.stucents.find((val) => val.name == ans.student);
+
+      if (!student) {
+        let name = new Student(ans.student);
+        persons.addStudent(name);
+
+        console.log(`Hello i am ${name.name}, and i am fine `);
+        console.log(persons.stucents);
+      }
+
+      if (student) {
+        console.log(`Hello i am ${student.name}, and i am fine..... `);
+        console.log(persons.stucents);
+      }
+    }
+  } while (true);
+};
+programmeStart(persons);
